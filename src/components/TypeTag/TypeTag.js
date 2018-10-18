1c4968cca7a05aea9909dcd72a5ab16fa54f6eee
Tyler Mercer <tyleralanmercer@gmail.com>
2018-10-17 22:45:11 -0600

    Add static type constants to TypeTag

--- src/components/TypeTag/TypeTag.js
+++ src/components/TypeTag/TypeTag.js
@@ -3,6 +3,14 @@ import React, { Component } from "react";
 import "./TypeTag.css";
 
 class TypeTag extends Component {
+  //Class-level constants, for use when instantiating TypeTags
+  static get TYPE_GAME() {
+    return "Game";
+  }
+  static get TYPE_DEMO() {
+    return "Demo";
+  }
+
   render() {
     return (
       <div className="TypeTagContainer">
