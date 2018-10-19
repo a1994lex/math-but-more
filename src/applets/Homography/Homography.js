8eff0106e1bea17b5898bdfccdc8fe1c8d29a9f8
Tyler Mercer <tyleralanmercer@gmail.com>
2018-10-18 19:54:30 -0600

    Add AppletHeader to Homography for testing

--- src/applets/Homography/Homography.js
+++ src/applets/Homography/Homography.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import AppletHeader from "../../components/AppletHeader";
 
 type Props = {};
 
 export default class Homography extends Component<Props> {
   render() {
-    return <div className="Homography">Homography</div>;
+    return (
+      <div className="Homography">
+        <AppletHeader />
+        <p>Homography</p>
+      </div>
+    );
   }
 }
