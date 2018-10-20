0f841487498040261d3a56a1014e8b21bbf9406c
Tyler Mercer <tyleralanmercer@gmail.com>
2018-10-20 16:40:32 -0600

    Add tags, description, and thumbnail to AppletCard

--- src/components/Card/AppletCard/AppletCard.js
+++ src/components/Card/AppletCard/AppletCard.js
@@ -1,18 +1,38 @@
 import React from 'react';
 
-import { LaunchButton } from '../..';
-import type { MathRoute } from '../../../models/routeTypes';
 import { Card } from '..';
+import { LaunchButton, SubjectTag, TypeTag } from '../..';
+import type { MathRoute } from '../../../models/routeTypes';
+import logo from "../../../logo.svg"
+
+import "./AppletCard.css"
 
 
 type Props = {
     route: MathRoute
 }
-function AppletCard( props ) {
+function AppletCard( props:Props ) {
+    //TODO: use actual description, thumbnail, tags, and tag callbacks
     return (
         <Card>
             <h3>{props.route.name}</h3>
-            <LaunchButton route={props.route}/>
+            <div className="AppletCard-desc-and-thumb">
+                <img src={logo} alt={props.route.name} className="AppletCard-thumb"/>
+                <div className="AppletCard-desc">Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</div>
+            </div>
+            <div className="AppletCard-button-and-tags">
+                <div className="AppletCard-tags">
+                    <TypeTag
+                        type={TypeTag.TYPE_GAME}
+                        onClick={()=>console.log("subject clicked")}
+                    />
+                    <SubjectTag
+                        subject="mySubject"
+                        onClick={()=>console.log("subject clicked")}
+                    />
+                </div>
+                <LaunchButton route={props.route}/>
+            </div>
         </Card>
     );
 }
