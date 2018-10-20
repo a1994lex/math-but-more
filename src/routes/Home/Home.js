eea7651e30e0362dabfb65331d839166306b2b85
Tyler Mercer <tyleralanmercer@gmail.com>
2018-10-20 09:15:42 -0600

    Clean up imports

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -7,14 +7,10 @@ import "./Home.css";
 type Props = {};
 export default class Home extends Component<Props> {
   render() {
-    console.log("rendering home");
     return (
       <div className="Home">
-        <Card route={appletRoutes[0]} />
-        <Card route={appletRoutes[1]} />
-        <Card route={appletRoutes[2]} />
-        <Card route={appletRoutes[3]} />
-        <Card route={appletRoutes[4]} />
+        {appletRoutes.map((route: MathRoute) => (
+          <Card key={route.name} route={route}/>))}
       </div>
     );
   }
