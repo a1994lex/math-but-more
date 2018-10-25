9dcca6f16840b964cbd261d4e27326d2306f19a1
Alex Powley <alex@infinidlearning.com>
2018-10-25 17:02:30 -0600

    Remove unneeded import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-import './main.less'
-
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
