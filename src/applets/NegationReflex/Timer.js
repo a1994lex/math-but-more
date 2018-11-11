54538637e2901f2aa03f28918978cca394b84ac8
BekaSpeed <ladyashengray@gmail.com>
2018-11-10 17:34:41 -0700

    workable negation applet

---
+++ src/applets/NegationReflex/Timer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+export default class Timer extends Component<Props> {
+	render() {
+		return <div className="Timer">I'm the Timer</div>
+	}
+}
