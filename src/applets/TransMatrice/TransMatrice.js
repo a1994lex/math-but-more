3fff8c636514e75704614f4409a78b65cbae9b73
Finn <jayspark57@gmail.com>
2018-11-11 19:57:33 -0700

    final touches to tmat applet for now

--- src/applets/TransMatrice/TransMatrice.js
+++ src/applets/TransMatrice/TransMatrice.js
@@ -45,12 +45,24 @@ export default class TransMatrice extends Component<Props> {
 		}
 		return (
 			<Container className="TransMatrice">
-				TransMatrice
 				<Row>
-					<span className="border border-dark" style={{ height: '700px', width: '700px' }}>
+					<span
+						className="border border-dark"
+						style={{
+							height: '700px',
+							width: '700px',
+							overflow: 'hidden',
+							backgroundColor: 'rgba(255, 255, 255, 0.8',
+						}}>
 						<img src={pacman} style={pman} />
 					</span>
-					<Col>
+					<Col
+						style={{
+							paddingLeft: '90px',
+							paddingRight: '90px',
+							paddingTop: '90px',
+							backgroundColor: '#ffcc66',
+						}}>
 						<form onSubmit={this.handleSubmit}>
 							<label>
 								Translate x:
@@ -69,14 +81,27 @@ export default class TransMatrice extends Component<Props> {
 								/>
 							</label>
 							<label>
-								Rotate:
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rotate:
 								<input type="text" value={this.state.rot} onChange={this.handleChangeRotate} />
 							</label>
 							<label>
-								Scale:
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Scale:
 								<input type="text" value={this.state.scale} onChange={this.handleChangeScale} />
 							</label>
-							<input type="submit" value="Submit" />
+							<Row style={{ marginLeft: '190px' }}>
+								<button style={{ backgroundColor: '#aaff66' }}>Submit</button>
+							</Row>
+							<Row>
+								<br />
+								Every image can be represented by a series of points that genreally look like:
+								Point(x, y) these points can be manipulated by multiplying them with data structures
+								called matrices. The simplest things you can do with an image and a matrix are
+								moving it (Translation), turning it (Rotation) and making it bigger or smaller
+								(Scaling). In the image above try warping the image with the input fields
+								<br />
+								<br /> **Use 50-500 for translations, any number for rotations(0-360 is a good place
+								to start), and 1-3 for scaling**.
+							</Row>
 						</form>
 					</Col>
 				</Row>
