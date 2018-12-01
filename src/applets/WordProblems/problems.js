7751c458158d3ff5b0ff1c554d3de830e3af6325
Finn <jayspark57@gmail.com>
2018-12-01 13:40:06 -0700

    2 more problems

--- src/applets/WordProblems/problems.js
+++ src/applets/WordProblems/problems.js
@@ -14,4 +14,14 @@ export const problems = [
 			'You collect Fortnite Dances. You had 13 this morning and have earned 5 today. How many do you have now?',
 		answer: 18,
 	},
+	{
+		problem:
+			'Johnny and Debbie both love pokemon and collect cards. When they meet up to trade Johnny gives Debbie 3 of his Charizard cards for 2 of her Dittos. If combined they had 62 cards and Debbie walked away with 17 how many cards did Johnny have before the traded?',
+		answer: 46,
+	},
+	{
+		problem:
+			'Kendall and Denise both like raisins, a lot. Kendall can eat 4 boxes in 3 hours and Denise can eat 1 box in 1 hours.  How many hours will it take for denise to eat more raisins than Kendall?',
+		answer: 5,
+	},
 ]
