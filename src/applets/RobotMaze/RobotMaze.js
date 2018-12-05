8b9a7f751003f0bf1092d67c2b54144db1b4b121
Kyle March <kcmarch23@gmail.com>
2018-12-04 17:41:51 -0700

    initial stylings

--- src/applets/RobotMaze/RobotMaze.js
+++ src/applets/RobotMaze/RobotMaze.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import './RobotMaze.css'
 
 type Props = {}
 
 export default class RobotMaze extends Component<Props> {
 	render() {
-		return <iframe className="RobotMaze" src="http://kylemarchbyu.com:8080/" width="90%" height="850"></iframe>
+		return(
+			<div class="maze">
+				<iframe className="RobotMaze" src="http://kylemarchbyu.com:8080/" border="none"></iframe>
+			</div>
+		) 
 	}
 }
